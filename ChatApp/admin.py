9978b4ecb46307a0053ec4fb18a69736549df00a
HJMakodiya <hemalimakodiya@gmail.com>
2021-03-20 17:20:47 +0530

    initial commit

---
+++ ChatApp/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from ChatApp.models import Message
+
+# Register your models here.
+admin.site.register(Message)
---
+++ ChatApp/views.py
@@ -0,0 +1,93 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.http.response import JsonResponse, HttpResponse
+from django.shortcuts import render, redirect
+from ChatApp.forms import SignUpForm
+from ChatApp.models import Message
+from ChatApp.serializers import MessageSerializer, UserSerializer
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
+# Create your views here.
+def index(request):
+    if request.user.is_authenticated:
+        return redirect('chats')
+    if request.method == 'GET':
+        return render(request, 'chat/index.html', {})
+    if request.method == "POST":
+        username, password = request.POST['username'], request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_staff == False and user.is_superuser == False:
+                login(request, user)
+        else:
+            return HttpResponse('{"error": "User does not exist"}')
+        return redirect('chats')
+
+
+def register_view(request):
+    """
+    Render registration template
+    """
+    if request.method == 'POST':
+        print("working1")
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user.set_password(password)
+            user.save()
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    if user.is_staff == False and user.is_superuser == False:
+                        login(request, user)
+                        return redirect('chats')
+    else:
+        print("working2")
+        form = SignUpForm()
+    template = 'chat/register.html'
+    context = {'form': form}
+    return render(request, template, context)
+
+
+def chat_view(request):
+    if not request.user.is_authenticated:
+        return redirect('index')
+    if request.method == "GET":
+        return render(request, 'chat/chat.html',
+                      {'users': User.objects.exclude(username=request.user.username).filter(is_staff=False)})
+
+
+def message_view(request, sender, receiver):
+    if not request.user.is_authenticated:
+        return redirect('index')
+    if request.method == "GET":
+        return render(request, "chat/messages.html",
+                      {'users': User.objects.exclude(username=request.user.username).filter(is_staff=False),
+                       'receiver': User.objects.get(id=receiver),
+                       'messages': Message.objects.filter(sender_id=sender, receiver_id=receiver) |
+                                   Message.objects.filter(sender_id=receiver, receiver_id=sender)})
+
+
+@csrf_exempt
+def message_list(request, sender=None, receiver=None):
+    """
+    List all required messages, or create a new message.
+    """
+    if request.method == 'GET':
+        messages = Message.objects.filter(sender_id=sender, receiver_id=receiver, is_read=False)
+        serializer = MessageSerializer(messages, many=True, context={'request': request})
+        for message in messages:
+            message.is_read = True
+            message.save()
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = MessageSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
